12fdfaf0672efefd18f5cb371e96e59417c42d8c
Rontu22 <rontubarhoi01@gmail.com>
2022-03-03 12:26:26 +0530

    Added all

--- log_to_cw.js
+++ log_to_cw.js
@@ -41,3 +41,44 @@ function cloudWatchDescribeLogStreams(group) {
   });
 }
 // exports.cloudWatchDescribeLogStreams = cloudWatchDescribeLogStreams;
+let nextSequenceToken = null; // need this for sending log to AWS. Will update after each request.
+let eventsQueue = [];
+let interval = null;
+// use a queue to send log to couldWatch one at a time to avoid throttling
+async function startLogQueueToCloudWatch() {
+  if (interval == null) {
+    interval = setInterval(async () => {
+      if (eventsQueue.length == 0) {
+        clearInterval(interval);
+        interval = null;
+        return;
+      }
+      let event = eventsQueue.shift();
+      try {
+        console.log(event);
+        let res = await cloudWatchPutLogEvents(
+          [event],
+          "api",
+          "prod",
+          nextSequenceToken
+        );
+        nextSequenceToken = res.nextSequenceToken; // store the new sequence token
+      } catch (error) {
+        // to allow retry
+        console.log(error);
+      }
+    }, 1000);
+  }
+}
+async function log(message) {
+  if (nextSequenceToken == null) {
+    // just ran server, get the token from AWS
+    let res = await cloudWatchDescribeLogStreams("api");
+    nextSequenceToken = res.logStreams[0].uploadSequenceToken;
+  }
+  eventsQueue.push({
+    message: message,
+    timestamp: new Date().getTime(),
+  });
+  await startLogQueueToCloudWatch();
+}
