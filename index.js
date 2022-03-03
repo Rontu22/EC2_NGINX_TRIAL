ecc416331c39c0934f6b6866d3f1fca128606fa7
Rontu22 <rontubarhoi01@gmail.com>
2022-03-03 10:55:13 +0530

    Added all

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log("IN INDEX FILE : This is a test log");
 console.log("This is another line");
+
+import {
+  CloudWatchLogsClient,
+  AssociateKmsKeyCommand,
+} from "@aws-sdk/client-cloudwatch-logs";
