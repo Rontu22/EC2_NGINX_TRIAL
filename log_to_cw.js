52144383e799f6ae25156aa3072d2a1e824fcd39
Rontu22 <rontubarhoi01@gmail.com>
2022-03-03 12:11:59 +0530

    Added all

--- log_to_cw.js
+++ log_to_cw.js
@@ -1,1 +1,42 @@
 import AWS from "aws-sdk";
+
+AWS.config.region = "ap-south-1";
+async function getSSMParameterByName(name) {
+  let ssm = new AWS.SSM();
+  return new Promise((resolve, reject) => {
+    ssm.getParameter({ Name: name }, function (err, data) {
+      if (err) reject(err);
+      resolve(data.Parameter.Value);
+    });
+  });
+}
+exports.getSSMParameterByName = getSSMParameterByName;
+function cloudWatchPutLogEvents(events, group, stream, sequenceToken) {
+  return new Promise((resolve, reject) => {
+    const cloudwatchlogs = new AWS.CloudWatchLogs();
+    var params = {
+      logEvents: events,
+      logGroupName: group,
+      logStreamName: stream,
+      sequenceToken: sequenceToken,
+    };
+    cloudwatchlogs.putLogEvents(params, function (err, data) {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+}
+exports.cloudWatchPutLogEvents = cloudWatchPutLogEvents;
+function cloudWatchDescribeLogStreams(group) {
+  return new Promise((resolve, reject) => {
+    const cloudwatchlogs = new AWS.CloudWatchLogs();
+    var params = {
+      logGroupName: group,
+    };
+    cloudwatchlogs.describeLogStreams(params, function (err, data) {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+}
+exports.cloudWatchDescribeLogStreams = cloudWatchDescribeLogStreams;
