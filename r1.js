3820ecbbc32ea1746d42124d4ab1bad9e789edfa
Rontu22 <rontubarhoi01@gmail.com>
2022-03-03 11:53:56 +0530

    Added all

--- r1.js
+++ r1.js
@@ -1,11 +1,10 @@
 console.log("IN INDEX FILE : This is a test log");
 console.log("This is another line");
-
 import {
-  CloudWatchLogsClient,
   CloudWatchEventsClient,
   ActivateEventSourceCommand,
-} from "@aws-sdk/client-cloudwatch-logs";
+} from "@aws-sdk/client-cloudwatch-events";
+
 // a client can be shared by different commands.
 // const client = new CloudWatchEventsClient({ region: "ap-south-1" });
 
