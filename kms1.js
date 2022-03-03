8b4e107892cdcd800b0c767c3c2a76728d90ca03
Rontu22 <rontubarhoi01@gmail.com>
2022-03-03 13:49:57 +0530

    Added all

---
+++ kms1.js
@@ -0,0 +1,10 @@
+var params = {
+  kmsKeyId:
+    "arn:aws:kms:ap-south-1:307208830146:key/d28c7717-a0a7-4036-8464-4805d88a7ff8" /* required */,
+  logGroupName: "/var/aws/group1" /* required */,
+};
+cloudwatchlogs.associateKmsKey(params, function (err, data) {
+  if (err) console.log(err, err.stack);
+  // an error occurred
+  else console.log(data); // successful response
+});
