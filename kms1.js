83af161fd7da40bc50daebf61d0f54a4aaea2462
Rontu22 <rontubarhoi01@gmail.com>
2022-03-03 13:53:59 +0530

    Added all

--- kms1.js
+++ kms1.js
@@ -1,3 +1,7 @@
+import AWS from "aws-sdk";
+
+var cloudwatchlogs = new AWS.CloudWatchLogs({ apiVersion: "2014-03-28" });
+
 var params = {
   kmsKeyId:
     "arn:aws:kms:ap-south-1:307208830146:key/d28c7717-a0a7-4036-8464-4805d88a7ff8" /* required */,
