82faff3085e659922ad1a47f6be545b6a4869f2d
Rontu22 <rontubarhoi01@gmail.com>
2022-03-03 11:28:24 +0530

    Added all

--- test2.js
+++ test2.js
@@ -3,6 +3,11 @@ console.log("This is another line");
 
 import {
   CloudWatchLogsClient,
+  DescribeLogStreamsCommand,
   AssociateKmsKeyCommand,
 } from "@aws-sdk/client-cloudwatch-logs";
 const client = new CloudWatchLogsClient({ region: "ap-south-1" });
+
+const logGroupName = "/var/awslogs/cloudwatch";
+
+// const logStreamRes = await client.send(new )
